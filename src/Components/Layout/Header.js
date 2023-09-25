6580edb3f2a72dac82c61dec1ce6a107a048376b
Utkarsh <utkarsharma304@gmail.com>
2023-09-25 20:12:15 +0530

    Designed a food ordering web-application using React framework, firebase as our backend and React-context for passing our data through various components.

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react"
+
+import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton onClick={props.onShowCart} />
+        </header>
+        <div className={classes["main-image"]}>
+            <img src={mealsImage} alt="A Table full of delicious foods!" />
+        </div>
+    </Fragment>
+}
+
+export default Header;
